d4d5626966e485f297b4ee3ec72c3705f07d7522
Harsh Rajput <77964116+harshrajput688@users.noreply.github.com>
2025-07-23 20:33:00 +0530

    handled the notification event if I am in the app

--- AppYogi Task Management/NotificationDelegate.swift
+++ AppYogi Task Management/NotificationDelegate.swift
@@ -13,4 +13,8 @@ class NotificationDelegate: NSObject, ObservableObject, UNUserNotificationCenter
         }
         completionHandler()
     }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
+    }
 } 
